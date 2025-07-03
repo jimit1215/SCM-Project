0f7f28f886c75f2d43a3323d29141aa76db5abb3
Jimit S. Dagli <daglijimit566@gmail.com>
2025-07-03 11:28:40 +0530

    Initial commit

---
+++ contact.php
@@ -0,0 +1,115 @@
+
+<?php include("vheader.php");?>
+
+<div class="navbar-inner">
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+				    aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto text-center mr-xl-5">
+						<li class="nav-item  mr-lg-2 mb-lg-0 mb-2">
+							<a class="nav-link" href="index.php">Home
+								<span class="sr-only">(current)</span>
+							</a>
+						</li>
+				
+						<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+							<a class="nav-link" href="about.php">About</a>
+						</li>
+				
+						<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link" href="product.php">Product</a>
+					    </li>
+						
+						<li class="nav-item active mr-lg-2 mb-lg-0 mb-2">
+							<a class="nav-link" href="contact.php">Contact</a>
+						</li>
+						<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+							<a class="nav-link" href="login.php">Login</a>
+						</li>
+						<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+							<a class="nav-link" href="register.php">Register</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+
+<!-- //navigation -->
+
+	<!-- banner-2 -->
+	<div class="page-head_agile_info_w3l">
+
+	</div>
+	<!-- //banner-2 -->
+	<!-- page -->
+	<div class="services-breadcrumb">
+		<div class="agile_inner_breadcrumb">
+			<div class="container">
+				<ul class="w3_short">
+					<li>
+						<a href="index.html">Home</a>
+						<i>|</i>
+					</li>
+					<li>Contact Us</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- //page -->
+
+	<!-- contact -->
+	<div class="contact py-sm-5 py-4">
+		<div class="container py-xl-4 py-lg-2">
+			<!-- tittle heading -->
+			<h3 class="tittle-w3l text-center mb-lg-5 mb-sm-4 mb-3">
+				<span>C</span>ontact
+				<span>U</span>s
+			</h3>
+			<!-- //tittle heading -->
+			<div class="row contact-grids agile-1 mb-5">
+				<div class="col-sm-4 contact-grid agileinfo-6 mt-sm-0 mt-2">
+					<div class="contact-grid1 text-center">
+						<div class="con-ic">
+							<i class="fas fa-map-marker-alt rounded-circle"></i>
+						</div>
+						<h4 class="font-weight-bold mt-sm-4 mt-3 mb-3">Address</h4>
+						<p>nr.Sir BPTI Campus, Vidhyanagar, Bhavnagar, Gujarat 364002
+							<label>India.</label>
+						</p>
+					</div>
+				</div>
+				<div class="col-sm-4 contact-grid agileinfo-6 my-sm-0 my-4">
+					<div class="contact-grid1 text-center">
+						<div class="con-ic">
+							<i class="fas fa-phone rounded-circle"></i>
+						</div>
+						<h4 class="font-weight-bold mt-sm-4 mt-3 mb-3">Call Us</h4>
+						<p> +91 8160142221
+						</p>
+					</div>
+				</div>
+				<div class="col-sm-4 contact-grid agileinfo-6">
+					<div class="contact-grid1 text-center">
+						<div class="con-ic">
+							<i class="fas fa-envelope-open rounded-circle"></i>
+						</div>
+						<h4 class="font-weight-bold mt-sm-4 mt-3 mb-3">Email</h4>
+						<p>
+							<a href="mailto:info@example.com">project7777@gmail.com</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	
+	<!-- footer -->
+	<?php include("footer.php");?>
+
+	
---
+++ js/scroll.js
@@ -0,0 +1,20 @@
+(function(){
+
+function scroller() {
+
+  var scroll = $('div.scroll');// Sets the div with a class of scroll as a variable
+  
+  var height = scroll.height(); // Gets the height of the scroll div
+  
+  var topAdj = -height-100; 
+	
+	scroll.animate({
+		'top' : [topAdj, 'linear'] 
+	}, 10000, function(){
+		scroll.css('top', 0); //resets the top position of the Ul for the next cycle
+		scroller(); // Recalls the animation cycle to begin.
+	});}
+	
+scroller();
+
+})();
